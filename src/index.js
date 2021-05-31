a83120af9e8bf42b0c53bfd57471d6230f962339
sounaksaha <sounaksaha100@gmail.com>
2021-05-31 23:53:35 +0530

    project Setup complete

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    h1 > hee there!! < /h1> <
+    h2 > Going to Build my professional portfolio🚀 < /h2> <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
