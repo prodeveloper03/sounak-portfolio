77ac32f372628eae4e3962f9a1c8ff2c8cb27bc3
sounak saha <sounaksaha100@gmail.com>
2021-06-08 23:19:29 +0530

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+}from 'react-router-dom'
+
+import About from './Pages/About/About'
+import Home  from './Pages/Home/Home'
+import Projects  from './Pages/Projects/Projects'
+import Skills  from './Pages/Skills/Skils'
+import Contact  from './Pages/Contact/Contact'
+
 
 
 const App = () => {
     return (
 
-        <h1>Error solved</h1>
+        <Router>
+            <main>
+                <Route path = "/" exact>
+                    <Home/>
+                </Route>
+                <Route path = "/about" exact>
+                    <About/>
+                </Route>
+                <Route path = "/projects" exact>
+                    <Projects/>
+                </Route>
+                <Route path = "/skills" exact>
+                    <Skills/>
+                </Route>
+                <Route path = "/contact" exact>
+                    <Contact/>
+                </Route>
+
+
+            </main>
+        </Router>
             
 
 
