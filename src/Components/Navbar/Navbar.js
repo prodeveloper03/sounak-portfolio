3f84a5e0103a1a069ade3a69212e33e2d67838c8
sounak saha <sounaksaha100@gmail.com>
2021-06-09 12:36:50 +0530

    navbarmodified

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,9 +11,66 @@ const Navbar = () => {
           Sounak🖐
 
       </NavLink>
-      <button>
-        
+      <button className = "navbar-toggler"
+      type = "button"
+      data-toggle = "collapse"
+      data-target = "#navbarSupportedContent"
+      aria-controls = "navbarSupportedContent"
+      aria-expanded = "false"
+      aria-label = "Toggle navigation">
+        <i className = "fas fa-bars text-white">
+        </i>
       </button>
+
+      <div 
+      className = "collapse-navbar-collapse"
+      id = "navbarSupportedContent">
+        <ul className = "navbar-nav ml-auto">
+          <div className = "hori-selector">
+            <div className = "left"></div>
+            <div className = "right"></div>
+          </div>
+
+          <li className = "nav-item active">
+            <NavLink className = "nav-link"
+            to = "/" exact>
+              <i className = "fasfa-tachometer-alt">
+              </i>Home
+            </NavLink>
+          </li>
+          <li className = "nav-item">
+            <NavLink className = "nav-link"
+            to = "/about" exact>
+              <i className = "farfa-adress-book">
+              </i>About
+            </NavLink>
+          </li>
+          <li className = "nav-item">
+            <NavLink className = "nav-link"
+            to = "/projects" exact>
+              <i className = "farfa-adress-book">
+              </i>Projects
+            </NavLink>
+          </li>
+          <li className = "nav-item">
+            <NavLink className = "nav-link"
+            to = "/skills" exact>
+              <i className = "farfa-adress-book">
+              </i>Skills
+            </NavLink>
+          </li>
+          <li className = "nav-item">
+            <NavLink className = "nav-link"
+            to = "/contact" exact>
+              <i className = "farfa-adress-book">
+              </i>Contact me
+            </NavLink>
+          </li>
+
+        </ul>
+
+      </div>
+      
 
     </nav>
     
