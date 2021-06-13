ca0c8c1d368c2c81ba2331bc00adda5cfea9f770
sounak saha <sounaksaha100@gmail.com>
2021-06-13 22:25:22 +0530

    rebuild navbar

--- src/App.js
+++ src/App.js
@@ -1,68 +1,11 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Switch
-}from 'react-router-dom'
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar/Navbar"
 
-import About from './Pages/About/About'
-import Home  from './Pages/Home/Home'
-import Projects  from './Pages/Projects/Projects'
-import Skills  from './Pages/Skills/Skils'
-import Contact  from './Pages/Contact/Contact'
-import Navbar from './Components/Navbar/Navbar'
-
-
-
-const App = () => {
+function App () {
     return (
 
-        <Router>
-            <Navbar/>
-                <main>
-                <Switch>
-                <Route path = "/" exact>
-                        <Home/>
-                            
-                        <img src = "homeImg.jpg" width = "100%" height = "100%"/>
-                    </Route>
-                    <Route path = "/about" exact>
-                        <About/>
-                    </Route>
-                    <Route path = "/projects" exact>
-                        <Projects/>
-                    </Route>
-                    <Route path = "/skills" exact>
-                        <Skills/>
-                    </Route>
-                    <Route path = "/contact" exact>
-                        <Contact/>
-                    </Route>
-                    <Route path = "/home" exact>
-                        <Home/>
-                    </Route>
-                    <Route path = "/about" exact>
-                        <About/>
-                    </Route>
-                    <Route path = "/projects" exact>
-                        <Projects/>
-                    </Route>
-                    <Route path = "/skills" exact>
-                        <Skills/>
-                    </Route>
-                    <Route path = "/contact" exact>
-                        <Contact/>
-                    </Route>
-                
-
-                </Switch>
-                </main>
-        </Router>
-            
-
+        <Navbar/>
 
     );
 
