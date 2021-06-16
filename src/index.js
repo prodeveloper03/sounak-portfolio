18e5c4eee93a962486749a232d00d83e51d2f748
sounak saha <sounaksaha100@gmail.com>
2021-06-17 01:11:28 +0530

    React Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -8,7 +9,11 @@ ReactDOM.render(
 
     <React.StrictMode >
 
-    <App/>
+        <BrowserRouter>
+
+         <App/>
+
+        </BrowserRouter>
 
     </React.StrictMode>,
     document.getElementById('root')
