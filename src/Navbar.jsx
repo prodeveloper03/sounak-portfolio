9a6edec4f4342eb1b8467487adb6c44094ccdddc
sounak saha <sounaksaha100@gmail.com>
2021-06-17 12:41:50 +0530

    navbar updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -18,10 +18,29 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <a className="nav-link" href="#">
+                    Home 
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
+                  <a className="nav-link" href="#Home">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Home">
+                    Project
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Home">
+                    Skill
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Home">
+                    Contact
+                  </a>
                 </li>
                 
               </ul>
