98e43760344e283a2ccfa9f41f138f347dc704d3
sounak saha <sounaksaha100@gmail.com>
2021-06-17 12:09:56 +0530

    navbar imported

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Switch ,Route, Redirect} from 'react-router';
-import Home from './Home'
-import About from './About'
-import Project from './Project'
-import Skill from './Skill'
-import Contact from './Contact'
+import Navbar from "./Navbar"
+import Home from './Home';
+import About from './About';
+import Project from './Project';
+import Skill from './Skill';
+import Contact from './Contact';
 
 const App = () => {
   return(
-
+    <>
+        <Navbar />
         <Switch>
           <Route exact path = "/" component = {Home} />
           <Route exact path = "/About" component = {About} />
@@ -18,6 +21,8 @@ const App = () => {
           <Redirect to = "/"/>
           
         </Switch>
+
+      </>
     
   );
 };
