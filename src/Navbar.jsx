a4c4bc44e1f3f3c2dd4188570e14f56ae24b8f56
sounak saha <sounaksaha100@gmail.com>
2021-06-17 14:14:47 +0530

    navbar completd

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
+              <ul className="navbar-nav ms-auto">
                 <li className="nav-item active">
                   <a className="nav-link" href="#">
                     Home 
