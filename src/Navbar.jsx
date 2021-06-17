0e4e8d1545caf3e300045477557a453ef939e50d
sounak saha <sounaksaha100@gmail.com>
2021-06-17 12:35:09 +0530

    dropdown removed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -23,17 +23,7 @@ const Navbar = () => {
                 <li className="nav-item">
                   <a className="nav-link" href="#">Link</a>
                 </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
+                
               </ul>
               
             </div>
