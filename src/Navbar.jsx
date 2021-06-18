d562bb5c08e68d9cdca2bc270d19b5ad8b2f2eda
sounak saha <sounaksaha100@gmail.com>
2021-06-18 16:03:59 +0530

    navbarbuild 1.0

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -17,27 +17,29 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item active">
-                  <NavLink className="nav-link" to="/">
+                  <NavLink className="nav-link" to="/" exact activeClassName = "menu-active">
                     Home 
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/About">
+                  <NavLink className="nav-link" to="/About"
+                  activeClassName = "menu-active">
                     About
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/Project">
+                  <NavLink className="nav-link" to="/Project"
+                  activeClassName = "menu-active">
                     Project
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/Skills">
+                  <NavLink className="nav-link" to="/Skills" activeClassName = "menu-active">
                     Skill
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/contact">
+                  <NavLink className="nav-link" to="/contact" activeClassName = "menu-active">
                     Contact
                   </NavLink>
                 </li>
