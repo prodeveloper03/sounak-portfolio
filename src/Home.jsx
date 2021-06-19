8130f11ce9bcaa041917948bc2e63d354330f399
sounak saha <sounaksaha100@gmail.com>
2021-06-19 17:08:46 +0530

    landing page styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,13 +4,13 @@ import web from "../src/images/homeImg.jpg"
 
 const Home = () => {
   return(
-    <section id = "header" className = "">
+    <section id = "header" className = "d-flex align-items-center">
 
       <div className = "container-fluid nav_bg">
             <div className = 'row'>
               <div className = "col-10 mx-auto">
                 <div className = "row">
-                <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center f-column">
 
                       <h1>
                         Hello! I am<br/><strong className ="brand-name">Sounak Saha</strong>
