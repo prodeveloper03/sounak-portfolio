e3eb359a37edce540655b93b647f94706431f381
sounak saha <sounaksaha100@gmail.com>
2021-06-22 11:15:29 +0530

    Home.js updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,32 @@ import mobile from "../src/images/homeImg.svg"
 const Home = () => {
   return(
     <>
-        <section>
+        <section id = "header" className = "">
+        <div className = "container-fluid nav_bg">
+            <div className = 'row'>
+                <div className = "col-10 mx-auto">
+                  <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                    <h1>
+                      Hello I am
+                    </h1>
+                    <h1>
+                      <strong>Sounak Saha</strong>
+                    </h1>
+                    <h2 className = "my-3">
+                      APP AND WEB DEVELOPER
+                    </h2>
+
+                  </div>
+                  <div className = "mt-3">
+
+                      <a href = "https://stackoverflow.com/questions/47427696/mx-auto-not-moving-element-to-centre" >Download Resume</a>
+
+                  </div>
+
+
+                </div>
+            </div>
+         </div>
            
         </section>
     </>
