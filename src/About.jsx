3962b48d65645711d3787dbbaf7dad9125ea3ae7
sounak saha <sounaksaha100@gmail.com>
2021-08-31 11:44:46 +0530

    text changed

--- src/About.jsx
+++ src/About.jsx
@@ -5,37 +5,37 @@ import profile from "../src/images/profile.jpg"
 
 
 const About = () => {
-  return(
+  return (
     <>
-        <section id = "about" className = "d-flex align-item-center">
-        <div className = "container-fluid nav_bg bg-dark">
-            <div className = 'row'>
-                <div className = "col-10 mx-auto">
-                  <div className = "row">
-                    <div className = "col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                    
-                          <h1>
-                            <strong className = "about-name">About me</strong>
-                          </h1>
-                          <h2 className = "about-text">
-                                I am in my 3rd year of undergrad in B.tech Computer science & engineering at the Jaypee Institute of Information and Technology. I take the industrial problem as a challenge and I love to storm my brain to find a very optimal solution for that particular problem.
-                          </h2>
-                          
-                    </div>
-                    <div className = "col-lg-6 order-1 order-lg-2 profile-img">
+      <section id="about" className="d-flex align-item-center">
+        <div className="container-fluid nav_bg bg-dark">
+          <div className='row'>
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
 
-                      <img src = {profile} className = "img-fluid animated" alt = "home img" />
-                      
-              
-                        
+                  <h1>
+                    <strong className="about-name">About Me</strong>
+                  </h1>
+                  <h2 className="about-text">
+                    I am in my 3rd year of undergrad in B.tech Computer science & engineering at the Jaypee Institute of Information and Technology. I take the industrial problem as a challenge and I love to storm my brain to find a very optimal solution for that particular problem.
+                  </h2>
 
-                      </div>
-                    </div>
-                  </div>
+                </div>
+                <div className="col-lg-6 order-1 order-lg-2 profile-img">
+
+                  <img src={profile} className="img-fluid animated" alt="home img" />
+
+
+
+
+                </div>
+              </div>
             </div>
-         </div>
-           
-        </section>
+          </div>
+        </div>
+
+      </section>
     </>
   );
 }
